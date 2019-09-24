dca3cfc3898b2201ec9fc263dd227a4a89b07df0
Rachel Jones <rlbluegirl101@gmail.com>
2019-09-24 16:44:01 -0600

    Added text box and button to change first buttons color

--- web/TA01/ta01.js
+++ web/TA01/ta01.js
@@ -1,3 +1,6 @@
 function clicked() {
     document.getElementsByTagName("div") = "Clicked";
 }
+function changeColor() {
+    document.getElementsByClassName("pink").style.color = document.getElementsByName("color");
+}
