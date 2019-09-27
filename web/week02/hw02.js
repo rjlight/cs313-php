2933eb4e1a66ed9b9665cfe15b5bf662769ed336
Rachel Jones <rlbluegirl101@gmail.com>
2019-09-27 13:46:52 -0600

    added pictures

--- web/week02/hw02.js
+++ web/week02/hw02.js
@@ -7,3 +7,21 @@ function waf() {
 function muf() {
     document.getElementById("yay").value = "Muffins rock!";
 }
+function cook() {
+    document.getElementById("yay").value = "Cookies are good for the soul!";
+}
+function bread() {
+    document.getElementById("yay").value = "Banana bread warms my heart!";
+}
+function scone() {
+    document.getElementById("yay").value = "Another scone please!";
+}
+function whe() {
+    document.getElementById("yay").value = "Yum, wheat bread!";
+}
+function bag() {
+    document.getElementById("yay").value = "Bagels are the bomb!";
+}
+function roll() {
+    document.getElementById("yay").value = "Rolls are awesome!";
+}
--- web/week02/hw02.php
+++ web/week02/hw02.php
@@ -15,9 +15,22 @@
     Pinterest</h3>
 <p id="yay"></p>
 <center>
-    <img onmouseover="waf()" src="waffles.PNG" alt="waffles" id="waffles" class="solid"/>
+    <img onclick="waf()" src="waffles.PNG" alt="waffles" id="waffles" class="solid"/>
     <img onmouseover="pan()" src="pancakes.PNG" alt="Pancakes" id="pancakes" class="solid"/>
     <img onmouseover="muf()" src="muffins.PNG" alt="Muffins" id="muffins" class="solid"/>
+</center>
+<h3>When I need a break from school, I've been known to whip up a batch of chocolate chip cookies,
+    banana bread, or something new like pomegranate scones!</h3>
+<center>
+    <img onclick="cook()" src="cookies.PNG" alt="Cookies" id="cook" class="solid"/>
+    <img onmouseover="bread()" src="bread.PNG" alt="Banana Bread" id="bread" class="solid"/>
+    <img onmouseover="scone()" src="scone.PNG" alt="Scones" id="scone" class="solid"/>
+</center>
+<h3>When I have a bit more time, I love to make wheat bread, bagels or rolls</h3>
+<center>
+    <img onclick="whe()" src="wheat.PNG" alt="Wheat Bread" id="wheat" class="solid"/>
+    <img onmouseover="bag()" src="bagel.PNG" alt="Bagels" id="bagel" class="solid"/>
+    <img onmouseover="roll()" src="rolls.PNG" alt="Rolls" id="rolls" class="solid"/>
 </center>
 <p>Please see link for other 
 <a href="hw02pg2.php">assignments</a>
