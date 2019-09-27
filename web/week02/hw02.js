eeefb23ae0d224efa591b5c25ba438ed95026f8b
Rachel Jones <rlbluegirl101@gmail.com>
2019-09-27 14:13:14 -0600

    almost done

--- web/week02/hw02.js
+++ web/week02/hw02.js
@@ -1,27 +1,36 @@
 function pan() {
     document.getElementById("yay").innerHTML = "Go pancakes!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function waf() {
     document.getElementById("yay").innerHTML = "We love waffles!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function muf() {
     document.getElementById("yay").innerHTML = "Muffins rock!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function cook() {
     document.getElementById("yay").innerHTML = "Cookies are good for the soul!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function bread() {
     document.getElementById("yay").innerHTML = "Banana bread warms my heart!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function scone() {
     document.getElementById("yay").innerHTML = "Another scone please!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function whe() {
     document.getElementById("yay").innerHTML = "Yum, wheat bread!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function bag() {
     document.getElementById("yay").innerHTML = "Bagels are the bomb!";
+    document.getElementById("yay").style.visibility = "visible";
 }
 function roll() {
     document.getElementById("yay").innerHTML = "Rolls are awesome!";
+    document.getElementById("yay").style.visibility = "visible";
 }
