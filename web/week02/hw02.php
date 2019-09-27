d18c7a5d9bfb98ffe5eeb31a4d5c172ef85eed12
Rachel Jones <rlbluegirl101@gmail.com>
2019-09-27 12:42:36 -0600

    Adjuested formatting and colors

--- web/week02/hw02.php
+++ web/week02/hw02.php
@@ -9,7 +9,7 @@
 <h1>Welcome to Rachel's Homepage!</h1>
 <h2>This page is about baking, someting I love</h2>
     <p>Often the question is asked, "what do you live for?" While I live for many
-    things, I <i>wake up for</i> breakfast!</p>
+    things, I <i>wake up</i>for breakfast!</p>
 <h3>Normal mornings I eat oatmeal, pretty plain with just cinnemon. But on the 
     weekends I make pancakes, waffles, muffins, or whatever looks best on 
     Pinterest</h3>
@@ -17,9 +17,10 @@
 <img onmouseover="pan()" src="pancakes.PNG" alt="Pancakes" id="pancakes" class="solid"/>
 <img onmouseover="waf()" src="waffles.PNG" alt="waffles" id="waffles" class="solid"/>
 <img onmouseover="muf()" src="muffins.PNG" alt="Muffins" id="muffins" class="solid"/>
-<p>Please see link below for Assignments</p>
-<a href="hw02pg2.php">Assignments</a>
-<footer>Date and time below
+<p>Please see link for other 
+<a href="hw02pg2.php">assignments</a>
+</p>
+<footer>Date and time:
 <?php echo date("D M d, Y G:i a"); ?>
 </footer>
 </body>
