a9ccaffd33feecba4a066503e4f34733659d931e
Rachel Jones <rlbluegirl101@gmail.com>
2019-09-27 13:22:30 -0600

    image

--- web/week02/hw02.php
+++ web/week02/hw02.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 </head>
 <body>
-<h1>Welcome to Rachel's Homepage!</h1>
+<h1>Welcome to Rachel's Homepage</h1>
 <h2>This page is about baking, something I love</h2>
     <p>Often the question is asked, "what do you live for?" While I live for many
     things, I <ins>wake up</ins> for breakfast!</p>
