fde7a165a20eb6d59fe9125885080cd7294c65a1
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-01 15:36:37 -0600

    php

---
+++ web/TA03/ta03.php
@@ -0,0 +1,22 @@
+<?php
+echo 'Your name is '. $_POST["name"].'<br>';
+echo 'Your email is '. $_POST["email"].'<br>';
+echo 'Your major is '. $_POST["major"].'<br>';
+echo 'Your comment is '. $_POST["comments"].'<br>';
+echo '<a href="mailto:' .$_POST["email"]. ' ">'.$_POST["email"].'</a>';
+$continents = $_POST['continents'];
+  if(empty($continents)) 
+  {
+    echo("You didn't select any continent.");
+  } 
+  else
+  {
+    $N = count($continents);
+
+    echo("You selected $N continent: ");
+    for($i=0; $i < $N; $i++)
+    {
+      echo($N. ": ". $continents[$i] . "<br>");
+    }
+  }
+?>
