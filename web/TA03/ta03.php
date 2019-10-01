f7d825102797daf61995f72a6d2b84b1483c4a0a
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-01 15:51:16 -0600

    fixed email

--- web/TA03/ta03.php
+++ web/TA03/ta03.php
@@ -1,9 +1,8 @@
 <?php
-echo 'Your name is: '. $_POST["name"].'<br>';
-echo 'Your email is: '. $_POST["email"].'<br>';
-echo 'Your major is: '. $_POST["major"].'<br>';
-echo 'Your comment is: '. $_POST["comments"].'<br>';
-echo '<a href="mailto:' .$_POST["email"]. ' ">'.$_POST["email"].'</a>';
+echo 'Your name is: '. $_POST["name"]. '<br>';
+echo 'Your email is: <a href="mailto:' .$_POST["email"]. ' ">'.$_POST["email"].'</a><br>';
+echo 'Your major is: '. $_POST["major"]. '<br>';
+echo 'Your comment is: '. $_POST["comments"]. '<br>';
 
 $continents = $_POST['continents'];
   if(empty($continents)) 
