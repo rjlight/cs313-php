756d66953057e2f3d821ca3a7e0cc747786fbcca
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-01 15:47:05 -0600

    fix

--- web/TA03/ta03.php
+++ web/TA03/ta03.php
@@ -1,9 +1,7 @@
 <?php
-echo 'Your name is '. $_POST["name"].'<br>';
-echo 'Your email is '. $_POST["email"].'<br>';
-echo 'Your major is '. $_POST["major"].'<br>';
-echo 'Your comment is '. $_POST["comments"].'<br>';
+echo 'Your name is: '. $_POST["name"].'<br>';
+echo 'Your email is: '. $_POST["email"].'<br>';
+echo 'Your major is: '. $_POST["major"].'<br>';
+echo 'Your comment is: '. $_POST["comments"].'<br>';
 echo '<a href="mailto:' .$_POST["email"]. ' ">'.$_POST["email"].'</a>';
-$continents = $_POST['continents'];
-
 ?>
