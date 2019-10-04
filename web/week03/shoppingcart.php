edee50ad094fdc13c36a2e17c867b955e57562f4
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-04 10:50:18 -0600

    create second page

---
+++ web/week03/shoppingcart.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Prove Assignment 03</title>
+<link rel="stylesheet" type="text/css" href="week03.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+</head>
+<body id="page">
+<center><h1 class="center">Welcome to the University Bookstore</h1>
+<h2 class="center">Where you can find all your back-to-school necessities!</h2>
+<form action="week03.php" method="post">
+<h3>Please select the number of each item desired: </h3><br>
+<center><div>
+    <p>Items currently in your cart: <br>
+    <?php $_POST["hat"] ?>
+    <?php $_POST["scarf"] ?>
+    <?php $_POST["socks"] ?>
+    <?php $_POST["hoodie"] ?>
+</div></center>
+<input id="button" type="button" value="Return to item page">
+<input id="submit" type="submit" value="Proceed to checkout"></center>
+</form>
+</body>
+</html>
--- web/week03/week03.php
+++ web/week03/week03.php
@@ -10,16 +10,16 @@
 <form action="week03.php" method="post">
 <h3>Please select the number of each item desired: </h3><br>
 <center><div>
-    <input type="checkbox" name="items[]" value="Hat"> Hat $15.99<br>
+    <input type="checkbox" name="items[]" name="hat" value="Hat $15.99"> Hat $15.99<br>
     <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>
-    <input type="checkbox" name="items[]" value="Scarf" > Scarf $13.99<br>
+    <input type="checkbox" name="items[]" name="scarf" value="Scarf $13.99"> Scarf $13.99<br>
     <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>
-    <input type="checkbox" name="items[]" value="Socks"> Socks $9.99<br>
+    <input type="checkbox" name="items[]" name="socks" value="Socks $9.99"> Socks $9.99<br>
     <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>
-    <input type="checkbox" name="items[]" value="Hoodie"> Hoodie $34.99<br>
+    <input type="checkbox" name="items[]" name="hoodie" value="Hoodie $34.99"> Hoodie $34.99<br>
     <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br>
 </div></center>
-<input id="submit" type="submit" value="Proceed to checkout"></center>
+<input id="submit" type="submit" value="View my cart"></center>
 </form>
 <?php 
 
@@ -27,9 +27,11 @@
  session_start();
 
  //if (isset($_POST['Hat'])) { 
- //$_SESSION['Hat'] = $_POST['Hat'];
- echo "<h4>Hat added to cart!</h4>"
- //} 
+    //$_SESSION['Hat'] = $_POST['Hat'];
+    echo "<center><h4>Hat added to cart!</h4></center>"
+ //} else {
+     echo "<center><h4></h4></center>"
+ //}
 ?> 
 </body>
 </html>
