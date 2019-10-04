e4a4a795e30687db9d09003706bbee3ad272cd5c
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-04 11:03:30 -0600

    comment out buttons

--- web/week03/week03.php
+++ web/week03/week03.php
@@ -7,17 +7,17 @@
 <body id="page">
 <center><h1 class="center">Welcome to the University Bookstore</h1>
 <h2 class="center">Where you can find all your back-to-school necessities!</h2>
-<form action="week03.php" method="post">
+<form action="shoppingcart.php" method="post">
 <h3>Please select the number of each item desired: </h3><br>
 <center><div>
     <input type="checkbox" name="items[]"value="Hat $15.99"> Hat $15.99<br>
-    <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>
+  <!--  <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>-->
     <input type="checkbox" name="items[]" value="Scarf $13.99"> Scarf $13.99<br>
-    <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>
+   <!--  <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>-->
     <input type="checkbox" name="items[]" value="Socks $9.99"> Socks $9.99<br>
-    <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>
+   <!--  <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br><br>-->
     <input type="checkbox" name="items[]" value="Hoodie $34.99"> Hoodie $34.99<br>
-    <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br>
+   <!--  <input type="button" class="button" value="Add items to cart" onclick="addItem(Hat)"><br>-->
 </div></center>
 <input id="submit" type="submit" value="View my cart"></center>
 </form>
