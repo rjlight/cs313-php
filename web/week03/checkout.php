0b34895e4f374b9ea8573e9e1b9ffafed3b1c0e4
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-05 13:21:25 -0600

    text are

--- web/week03/checkout.php
+++ web/week03/checkout.php
@@ -10,7 +10,7 @@
 <form action="confirmation.php" method="post">
 <center><div>
   <p>Shipping address:</p>
-  <textarea name="address" rows="4" cols="50"></textarea><br>
+  <textarea name="address" rows="4" cols="10"></textarea><br>
 </div></center>
 <a id="button" href="shoppingcart.php">Back to shopping cart</a>
 <input id="submit" type="submit" value="Place order"></center>
