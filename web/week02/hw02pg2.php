d61ce93bbf7faef9ccfe7cede3752cd009bfc615
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-05 20:00:01 -0600

    added link to main page

--- web/week02/hw02pg2.php
+++ web/week02/hw02pg2.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" type="text/css" href="hw02.css">
 </head>
 <body>
-<h1>Page contents to be finished later</h1>
+<h1>Links to assignments can be found below:</h1>
+<a id="button" href="week03.php">Week03 Assignment</a>
 </body>
 </html>
