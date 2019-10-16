c40d16ecb26f5c41a2053b39afbca8c15d46ae66
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-16 09:43:44 -0600

    put border in its own div

--- web/MusicStudents/signIn.php
+++ web/MusicStudents/signIn.php
@@ -5,12 +5,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 </head>
 <body id="page">
-<h1 class="center">Please sign in to your account:</h1>
-<form action="listAccountNames.php" method="post">
-<div class="input">
-    Username: <input type="text" name="username"><br><br>
-    Password: <input type="text" name="password"><br><br>
+<div class="border">
+    <h1 class="center">Please sign-in to your account:</h1>
+    <form action="listAccountNames.php" method="post">
+    <div class="input">
+        Username: <input type="text" name="username"><br><br>
+        Password: <input type="text" name="password"><br><br>
+    </div>
+    <input id="submit" type="submit" value="Sign-In">
+    </form>
 </div>
-<input id="submit" type="submit" value="Sign In">
-</form>
 </html>
