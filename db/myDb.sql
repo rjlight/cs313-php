3bc6d3c14ffa6cbd38f33b6967dfc7ff0f9cc24e
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-17 16:15:44 -0600

    Update database to eliminate duplicate values

--- db/myDb.sql
+++ db/myDb.sql
@@ -45,3 +45,57 @@ JOIN account AS a
 ON s.id = a.student_id_1
 OR s.id = a.student_id_2
 WHERE a.username = 'lightclan';
+
+UPDATE student
+SET lesson_time='4:30 PM'
+WHERE id=2;
+
+<?php
+echo '<h1><strong>Scripture Resources</strong></h1>';
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+{
+  echo '<strong>' . $row['book'];
+  echo ' ' . $row['chapter'];
+  echo ':' . $row['verse'];
+  echo '-</strong> &quot' . $row['content'] . '&quot';
+  echo '<br/>';
+}
+
+echo '<form action=ta05.php method=POST>';
+echo 'Book: <input type=text name=book>';
+echo '<input type=submit value=Search>';
+echo '</form>';
+
+$book = $_POST['book'];
+
+foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book =' . '\''. $book . '\'') as $row)
+{
+  echo '<strong>' . $row['book'];
+  echo ' ' . $row['chapter'];
+  echo ':' . $row['verse'];
+  echo '-</strong> &quot' . $row['content'] . '&quot';
+  echo '<br/>';
+}
+?>
