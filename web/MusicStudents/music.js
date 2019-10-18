7bc94f35b7ead59ba7a5ef2447c96cb54ae75a33
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-18 16:19:09 -0600

    Adjusted visibility

--- web/MusicStudents/music.js
+++ web/MusicStudents/music.js
@@ -1,28 +1,36 @@
 function checkParent() { //to add an edit field for additional parents
     if (document.getElementsByName("parent2").checked == true) {
         document.getElementById("parent2").style.fontSize = 12px;
+        document.getElementById("parent2").style.visibility = visible;
     }
     else {
-        document.getElementById("parent2").style.fontSize = .001px;
+        document.getElementById("parent2").style.fontSize = .001px
+        document.getElementById("parent2").style.visibility = hidden;
     }
 }
 function checkStudent() { //to add an edit field for additional students
     if (document.getElementsByName("student2").checked == true) {
         document.getElementById("student2").style.fontSize = 12px;
+        document.getElementById("student2").style.visibility = visible;
     }
     else {
         document.getElementById("student2").style.fontSize = .001px;
+        document.getElementById("student2").style.visibility = hidden;
     }
     if (document.getElementsByName("student3").checked == true) {
         document.getElementById("student3").style.fontSize = 12px;
+        document.getElementById("student3").style.visibility = visible;
     }
     else {
         document.getElementById("student3").style.fontSize = .001px;
+        document.getElementById("student3").style.visibility = hidden;
     }
     if (document.getElementsByName("student4").checked == true) {
         document.getElementById("student4").style.fontSize = 12px;
+        document.getElementById("student4").style.visibility = visible;
     }
     else {
         document.getElementById("student4").style.fontSize = .001px;
+        document.getElementById("student4").style.visibility = hidden;
     }
 }
