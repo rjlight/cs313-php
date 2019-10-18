4cc9d4380bbec37dc16a67d998813fccbcafb100
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-18 16:45:31 -0600

    Adjusted Javascript to add room for stuff

--- web/MusicStudents/editAccount.php
+++ web/MusicStudents/editAccount.php
@@ -18,12 +18,6 @@
         Yes <input type="checkBox" name="parent2" onclick="checkParent()"> <br> 
 
         <div id="parent2">
-            <h3>Second Parent Information</h3>
-            Full Name: <input type="text" name="parentName2"><br><br>
-            Email Address: <input type="text" name="email2"><br><br>
-            Phone Number: <input type="number" name="phoneNumber2"><br><br>
-            Best Form of Contact (email, text, phone call, etc.): <br>
-                <input type="text" name="contact2"><br><br>
         </div>
         <h3>Student Information</h3>
         Full Name: <input type="text" name="studentName1"><br><br>
@@ -36,38 +30,10 @@
         Would you like to add another student? <br>
         Yes <input type="checkBox" name="student2" onclick="checkStudent()"> <br>
         <div id="student2">
-            <h3>Student Information</h3>
-            Full Name: <input type="text" name="studentName2"><br><br>
-            Age: <input type="number" name="age2"><br><br>
-            Instrument: <input type="text" name="instrument2"><br><br>
-            Have you had lessons before? If yes, for how long? <br>
-            <input type="text" name="lessonsBefore2"><br><br>
-            Lesson Day: <input type="text" name="lessonDay2"><br><br>
-            Lesson Time: <input type="text" name="lessonTime2"><br><br>
-            Would you like to add another student? <br>
-            Yes <input type="checkBox" name="student3" onclick="checkStudent()"> <br>
         </div>
         <div id="student3">
-            <h3>Third Student Information</h3>
-            Full Name: <input type="text" name="studentName3"><br><br>
-            Age: <input type="number" name="age3"><br><br>
-            Instrument: <input type="text" name="instrument3"><br><br>
-            Have you had lessons before? If yes, for how long? <br>
-            <input type="text" name="lessonsBefore3"><br><br>
-            Lesson Day: <input type="text" name="lessonDay3"><br><br>
-            Lesson Time: <input type="text" name="lessonTime3"><br><br>
-            Would you like to add another student? <br>
-            Yes <input type="checkBox" name="student4" onclick="checkStudent()"> <br>
         </div>
         <div id="student4">
-            <h3>Fourth Student Information</h3>
-            Full Name: <input type="text" name="studentName4"><br><br>
-            Age: <input type="number" name="age4"><br><br>
-            Instrument: <input type="text" name="instrument4"><br><br>
-            Have you had lessons before? If yes, for how long? <br>
-            <input type="text" name="lessonsBefore4"><br><br>
-            Lesson Day: <input type="text" name="lessonDay4"><br><br>
-            Lesson Time: <input type="text" name="lessonTime4"><br>
         </div>
         
     </form>
--- web/MusicStudents/music.js
+++ web/MusicStudents/music.js
@@ -1,36 +1,33 @@
 function checkParent() { //to add an edit field for additional parents
     if (document.getElementsByName("parent2").checked == true) {
-        document.getElementById("parent2").style.fontSize = 12px;
-        document.getElementById("parent2").style.visibility = visible;
+        var txt = "<h3>Second Parent Information</h3> Full Name: <input type=\"text\" name=\"parentName2\"><br><br> Email Address: <input type=\"text\" name=\"email2\"><br><br> Phone Number: <input type=\"number\" name=\"phoneNumber2\"><br><br> Best Form of Contact (email, text, phone call, etc.): <br> <input type=\"text\" name=\"contact2\"><br><br>";
+        document.getElementById("parent2").innerHTML = txt;
     }
     else {
-        document.getElementById("parent2").style.fontSize = .001px
-        document.getElementById("parent2").style.visibility = hidden;
+        document.getElementById("parent2").innerHTML = "";
     }
 }
 function checkStudent() { //to add an edit field for additional students
+    var txt;
     if (document.getElementsByName("student2").checked == true) {
-        document.getElementById("student2").style.fontSize = 12px;
-        document.getElementById("student2").style.visibility = visible;
+        txt = "<h3>Student Information</h3> Full Name: <input type=\"text\" name=\"studentName2\"><br><br> Age: <input type=\"number\" name=\"age2\"><br><br>Instrument: <input type=\"text\" name=\"instrument2\"><br><br> Have you had lessons before? If yes, for how long? <br> <input type=\"text\" name=\"lessonsBefore2\"><br><br> Lesson Day: <input type=\"text\" name=\"lessonDay2\"><br><br> Lesson Time: <input type=\"text\" name=\"lessonTime2\"><br><br>  Would you like to add another student? <br> Yes <input type=\"checkBox\" name=\"student3\" onclick=\"checkStudent()\"> <br>";
+        document.getElementById("student2").innerHTML = txt;
     }
     else {
-        document.getElementById("student2").style.fontSize = .001px;
-        document.getElementById("student2").style.visibility = hidden;
+        document.getElementById("student2").innerHTML = "";
     }
     if (document.getElementsByName("student3").checked == true) {
-        document.getElementById("student3").style.fontSize = 12px;
-        document.getElementById("student3").style.visibility = visible;
+        txt = "<h3>Third Student Information</h3>Full Name: <input type=\"text\" name=\"studentName3\"><br><br>Age: <input type=\"number\" name=\"age3\"><br><br>Instrument: <input type=\"text\" name=\"instrument3\"><br><br> Have you had lessons before? If yes, for how long? <br><input type=\"text\" name=\"lessonsBefore3\"><br><br> Lesson Day: <input type=\"text\" name=\"lessonDay3\"><br><br> Lesson Time: <input type=\"text\" name=\"lessonTime3\"><br><br> Would you like to add another student? <br> Yes <input type=\"checkBox\" name=\"student4\" onclick=\"checkStudent()\"> <br>";
+        document.getElementById("student3").innerHTML = txt;
     }
     else {
-        document.getElementById("student3").style.fontSize = .001px;
-        document.getElementById("student3").style.visibility = hidden;
+        document.getElementById("student3").innerHTML = "";
     }
     if (document.getElementsByName("student4").checked == true) {
-        document.getElementById("student4").style.fontSize = 12px;
-        document.getElementById("student4").style.visibility = visible;
+        txt = "<h3>Fourth Student Information</h3> Full Name: <input type=\"text\" name=\"studentName4\"><br><br> Age: <input type=\"number\" name=\"age4\"><br><br> Instrument: <input type=\"text\" name=\"instrument4\"><br><br> Have you had lessons before? If yes, for how long? <br> <input type=\"text\" name=\"lessonsBefore4\"><br><br> Lesson Day: <input type=\"text\" name=\"lessonDay4\"><br><br> Lesson Time: <input type=\"text\" name=\"lessonTime4\"><br>";
+        document.getElementById("student4").innerHTML = txt;
     }
     else {
-        document.getElementById("student4").style.fontSize = .001px;
-        document.getElementById("student4").style.visibility = hidden;
+        document.getElementById("student4").innerHTML = "";
     }
 }
