7ff2eeab10d9cf5c6fac22bd912a69759ce09337
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-18 17:35:49 -0600

    replaced all double q with single q

--- web/MusicStudents/music.js
+++ web/MusicStudents/music.js
@@ -1,6 +1,6 @@
 function checkParent() { //to add an edit field for additional parents
     if (document.getElementsByName("parent2")[0].checked == true) { 
-        var txt = "<h3>Second Parent Information</h3> Full Name: <input type=\"text\" name=\"parentName2\"><br><br> Email Address: <input type=\"text\" name=\"email2\"><br><br> Phone Number: <input type=\"number\" name=\"phoneNumber2\"><br><br> Best Form of Contact (email, text, phone call, etc.): <br> <input type=\"text\" name=\"contact2\"><br><br>";
+        var txt = "<h3>Second Parent Information</h3> Full Name: <input type='text' name='parentName2'><br><br> Email Address: <input type='text' name='email2'><br><br> Phone Number: <input type='number' name='phoneNumber2'><br><br> Best Form of Contact (email, text, phone call, etc.): <br> <input type='text' name='contact2'><br><br>";
         document.getElementById("parent2").innerHTML = txt;
     }
     else {
@@ -13,10 +13,10 @@ function checkStudent() { //to add an edit field for additional students
         txt1 = "<h3>Student Information</h3> Full Name: <input type='text' name='studentName2'><br><br> Age: <input type='number' name='age2'><br><br> Instrument: <input type='text' name='instrument2'><br><br> Have you had lessons before? If yes, for how long? <br> <input type='text' name='lessonsBefore2'><br><br> Lesson Day: <input type='text' name='lessonDay2'><br><br> Lesson Time: <input type='text' name='lessonTime2'><br><br>  Would you like to add another student? <br> Yes <input type='checkBox' name='student3' onclick='checkStudent()'> <br>";
         document.getElementById("student2").innerHTML = txt1;
         if (document.getElementsByName("student3")[0].checked == true) {
-            txt2 = "<h3>Third Student Information</h3>Full Name: <input type=\"text\" name=\"studentName3\"><br><br>Age: <input type=\"number\" name=\"age3\"><br><br>Instrument: <input type=\"text\" name=\"instrument3\"><br><br> Have you had lessons before? If yes, for how long? <br><input type=\"text\" name=\"lessonsBefore3\"><br><br> Lesson Day: <input type=\"text\" name=\"lessonDay3\"><br><br> Lesson Time: <input type=\"text\" name=\"lessonTime3\"><br><br> Would you like to add another student? <br> Yes <input type=\"checkBox\" name=\"student4\" onclick=\"checkStudent()\"> <br>";
+            txt2 = "<h3>Third Student Information</h3>Full Name: <input type='text' name='studentName3'><br><br>Age: <input type='number' name='age3'><br><br>Instrument: <input type='text' name='instrument3'><br><br> Have you had lessons before? If yes, for how long? <br><input type='text' name='lessonsBefore3'><br><br> Lesson Day: <input type='text' name='lessonDay3'><br><br> Lesson Time: <input type='text' name='lessonTime3'><br><br> Would you like to add another student? <br> Yes <input type='checkBox' name='student4' onclick='checkStudent()'> <br>";
             document.getElementById("student3").innerHTML = txt2;
             if (document.getElementsByName("student4")[0].checked == true) {
-                txt3 = "<h3>Fourth Student Information</h3> Full Name: <input type=\"text\" name=\"studentName4\"><br><br> Age: <input type=\"number\" name=\"age4\"><br><br> Instrument: <input type=\"text\" name=\"instrument4\"><br><br> Have you had lessons before? If yes, for how long? <br> <input type=\"text\" name=\"lessonsBefore4\"><br><br> Lesson Day: <input type=\"text\" name=\"lessonDay4\"><br><br> Lesson Time: <input type=\"text\" name=\"lessonTime4\"><br>";
+                txt3 = "<h3>Fourth Student Information</h3> Full Name: <input type='text' name='studentName4'><br><br> Age: <input type='number' name='age4'><br><br> Instrument: <input type='text' name='instrument4'><br><br> Have you had lessons before? If yes, for how long? <br> <input type='text' name='lessonsBefore4'><br><br> Lesson Day: <input type='text' name='lessonDay4'><br><br> Lesson Time: <input type='text' name='lessonTime4'><br>";
                 document.getElementById("student4").innerHTML = txt3;
             }
             else {
