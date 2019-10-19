777fe1f66c8e79e289838d94b6ab63878d4cee8a
Rachel Jones <rlbluegirl101@gmail.com>
2019-10-18 18:09:17 -0600

    adjust database again

--- db/myDb.sql
+++ db/myDb.sql
@@ -27,20 +27,20 @@ parent_id_1         int NOT NULL REFERENCES parent(id),
 parent_id_2         int REFERENCES parent(id),
 PRIMARY KEY(username)
 );
-INSERT INTO account (username, password, student_id_1, parent_id_1) 
-VALUES ('lightclan', 'welovelight', 1, 1);
+INSERT INTO account (username, password, student_id_1, student_id_2, student_id_3, parent_id_1) 
+VALUES ('lightclan', 'welovelight', 1, 2, 3, 1);
 
 INSERT INTO student (name, age, instrument, lesson_before, lesson_day, lesson_time) 
 VALUES ('Leah Light', 14, 'violin', 'Yes, 4 years', 'Monday', '3:30 PM');
 
 INSERT INTO parent (name, email, phone_number, best_form_contact) 
-VALUES ('Marie Light', 'fakeemail@gmail.com', 333-333-9333, 'texting, please');
+VALUES ('Marie Light', 'fakeemail@gmail.com', '333-333-9333', 'texting, please');
 
 UPDATE account
 SET student_id_3=3; 
 
 UPDATE parent
-SET phone_number=;
+SET phone_number="333-333-9333";
 
 ALTER TABLE parent 
 MODIFY (phone_number VARCHAR(13) NOT NULL);
