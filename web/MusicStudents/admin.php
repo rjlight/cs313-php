ecc9c0d79a23dbe8dbe9a2206f8ef63b8bcde883
Rachel Jones <rlbluegirl101@gmail.com>
2019-11-01 09:19:19 -0600

    fixed admin account

--- web/MusicStudents/admin.php
+++ web/MusicStudents/admin.php
@@ -32,6 +32,8 @@
             echo 'Error!: ' . $ex->getMessage();
             die();
         }
+        $username = $_POST['username'];
+        $password = $_POST['password'];
         $count = 0; //to count the users
 
         if($username="admin" && $password="admin789") {
@@ -40,8 +42,8 @@
             {
                 $count++; //we will increment the count for each user
                 echo '<p>' . $count . ':<br>'; //Which account is which
-                echo '<p>\tUsername: ' . $row['username'] .'</p>';
-                echo '<p>\tPassword: ' . $row['password'] .'</p>';
+                echo '<p><span>Username: ' . $row['username'] .'</span></p>';
+                echo '<p><span>Password: ' . $row['password'] .'</span></p>';
                 $_SESSION['parent_id_1' . '\''. $username . '\''] = $row['parent_id_1'];
                 $_SESSION['parent_id_2' . '\''. $username . '\''] = $row['parent_id_2'];
                 $_SESSION['student_id_1' . '\''. $username . '\''] = $row['student_id_1'];
@@ -50,15 +52,15 @@
                 $_SESSION['student_id_4' . '\''. $username . '\''] = $row['student_id_4'];
             }
             $userArray = new SplFixedArray($count); //create a userArray with the size of the # of users
-            //$userArray = array();
             $index = 0;
             foreach ($db->query('SELECT username FROM account') as $row) {
                 $userArray[$index] = $row['username']; //we'll put each user into the userArray
                 $index++;                             //increment the index
             }
             $_SESSION['userArray'] = $userArray;      //now we have an array of users
-            echo 'Please enter the account number you would like to see the details for: <br><input type=text name=account>';
-            echo '<input type=submit name=submit>';  //this is where we'll get the account # ^ 
+            echo '<p>Please enter the account number you would like to see the details for: /p>'
+            . '<br><input type=text name=account>';
+            echo '<input type=submit name=submit value=Details>';  //this is where we'll get the account # ^ 
         } else {
             echo "Sorry, you are not an administrator for this site<br>
             Please return to the <a href=\'signIn.php\'>sign-in page</a>";
