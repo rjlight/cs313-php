6bffe6d5bd3009e165d916e8e32f62a7d4466343
Rachel Jones <rlbluegirl101@gmail.com>
2019-11-01 16:08:14 -0600

    more error check

--- web/MusicStudents/music.js
+++ web/MusicStudents/music.js
@@ -59,6 +59,8 @@ function checkPassword() {
 }
 function checkNumber(num) {
     var test = document.getElementById("accountNum").value;
+    document.getElementById("error3").innerHTML = num;
+    document.getElementById("error3").style.visibility = "visible";
     if (test > num) {
         document.getElementById("error3").style.visibility = "visible";
     } else {
