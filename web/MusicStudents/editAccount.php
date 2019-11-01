a2900d15facc42cf54dbd047d6aba7e7a8fc620f
Rachel Jones <rlbluegirl101@gmail.com>
2019-11-01 13:29:04 -0600

    added required to main student and parent

--- web/MusicStudents/editAccount.php
+++ web/MusicStudents/editAccount.php
@@ -11,27 +11,27 @@
     <form action="accountCreated.php" method="post">
         <h3>Account Information</h3>
         Username: <input type="text" name="username" required><br><br>
-        Password: <input type="password" name="password" id="password" oninput="checkPassword()"><br><br>
-        Re-Enter Password: <input type="password" name="password2" id="password2" oninput="checkPassword()"><br>
+        Password: <input type="password" name="password" id="password" oninput="checkPassword()" required><br><br>
+        Re-Enter Password: <input type="password" name="password2" id="password2" oninput="checkPassword()" required><br>
         <p id="error1" style="visibility:hidden;">*Error: Passwords don't match!</p>
         <h3>Parent Information</h3>
-        Full Name: <input type="text" name="parentName1"><br><br>
-        Email Address: <input type="text" name="email"><br><br>
-        Phone Number: <input type="text" name="phoneNumber"><br><br>
+        Full Name: <input type="text" name="parentName1" required><br><br>
+        Email Address: <input type="text" name="email" required><br><br>
+        Phone Number: <input type="text" name="phoneNumber" required><br><br>
         Best Form of Contact (email, text, phone call, etc.): <br>
-            <input type="text" name="contact"><br><br>
+            <input type="text" name="contact" required><br><br>
         Would you like to add another parent? <br>
         Yes <input type="checkBox" name="parent2" onclick="checkParent()"> <br> 
 
         <div id="parent2"></div>
         <h3>Student Information</h3>
-        Full Name: <input type="text" name="studentName1"><br><br>
-        Age: <input type="number" name="age1"><br><br>
-        Instrument: <input type="text" name="instrument1"><br><br>
+        Full Name: <input type="text" name="studentName1" required><br><br>
+        Age: <input type="number" name="age1" required><br><br>
+        Instrument: <input type="text" name="instrument1" required><br><br>
         Have you had lessons before? If yes, for how long? <br>
-        <input type="text" name="lessonsBefore1"><br><br>
-        Lesson Day: <input type="text" name="lessonDay1"><br><br>
-        Lesson Time: <input type="text" name="lessonTime1"><br><br>
+        <input type="text" name="lessonsBefore1" required><br><br>
+        Lesson Day: <input type="text" name="lessonDay1" required><br><br>
+        Lesson Time: <input type="text" name="lessonTime1" required><br><br>
         Click to add another student: <br>
         <input type="button" name="student2" onclick="addStudent()" value="Add Student"> <br>
         <div id="student2">
