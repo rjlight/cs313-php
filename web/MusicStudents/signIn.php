3e6ca0500dc714dba4cb6c99e2f85b251cd1431b
Rachel Jones <rlbluegirl101@gmail.com>
2019-11-01 09:35:05 -0600

    Adding button to link

--- web/MusicStudents/signIn.php
+++ web/MusicStudents/signIn.php
@@ -14,7 +14,7 @@
         <p>Don't have an account yet? Make one <a href="editAccount.php">here</a></p>
     </div>
     <input id="submit" type="submit" value="Sign-In">
-    <a href="adminSignIn.php">admin</a>
+    <a href="adminSignIn.php"><button>Admin</button></a>
     </form>
     <form action="adminSignIn.php">
         <input id="button" type="submit" value="Admin">
